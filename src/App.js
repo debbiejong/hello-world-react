7d4803050bf85346d27fb67467728c650fa289a8
Debbie Jong <surrealstardust@gmail.com>
2018-01-04 17:57:51 +0900

    add HelloWorld component with option to frenchify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 const App = () => {
-  return (<div className="App">Hello World!</div>);
+  return (
+    <div className="App">
+      <HelloWorld name="Debbie"/>
+      <HelloWorld name="Diane"/>
+    </div>
+    );
 };
 
 export default App;
