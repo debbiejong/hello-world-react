e3aa4883fcac1f1b2d060be74af392983b641af9
Debbie Jong <surrealstardust@gmail.com>
2018-01-05 11:12:47 +0900

    create greeting adder component

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import './HelloWorldList.css';
 
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 
 class HelloWorldList extends Component {
   constructor(props) {
     super(props);
-    this.state = {greetings: ['Debonaire', 'Dyan']};
+    this.state = {greetings: ['Debonaire', 'Dyan', 'Julie']};
+    this.addGreeting = this.addGreeting.bind(this);
+  }
+
+  addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
+  }
+
+  renderGreetings() {
+    return this.state.greetings.map(name => (
+      <HelloWorld key={name} name={name}/>
+    ));
   }
 
   render() {
     return (
       <div className="HelloWorldList">
-        <HelloWorld name="Debonaire"/>
-        <HelloWorld name="Dyan"/>
+      <AddGreeter addGreeting= {this.addGreeting}/>
+        {this.renderGreetings()}
       </div>
     );
   }
